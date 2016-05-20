dc974aa9a03e5a4af0502b1f1e77c448a054ed82
Taitanguo <uvasiyuan@gmail.com>
2016-05-20 00:31:18 -0400

    nested list and join

---
+++ nested list.py
@@ -0,0 +1,12 @@
+'marksheet = []
+for _ in range(0, int(input())):
+    marksheet.append([input(), float(input())]) #read two lines into one cube in a list
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1] # find the second hightest score in all students
+print('\n'.join(a for a,b int sorted(marksheet) if b == second_highest))'
+
+#second version
+n = int(raw_input())
+marks = [[raw_input(), float(raw_input())] for i in  xrange(n)]
+scores = sorted({s[1] for s in marks})
+result = sorted(s[0] for s in marks if s[1] == scores[1])
+print '\n'.join(result)
